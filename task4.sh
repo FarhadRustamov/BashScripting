7d2921bb61d6019bb8eafebc20603418e8994df0
Farhad Rustamov <farhad.rustamov.dev@gmail.com>
2024-11-14 13:09:09 +0400

    All 4 tasks are ready

---
+++ task4.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+FILE=""
+SEARCH=""
+
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		--file)
+			FILE=$2
+			shift
+			shift
+			;;
+		--search)
+			SEARCH=$2
+			shift
+			shift
+			;;
+		*)
+			echo "Error argument $2 not supported"
+			exit 1
+	esac
+done
+
+NUMBER_OF_LINES=$(grep -o "$SEARCH" "$FILE" | wc -l)
+if (( NUMBER_OF_LINES == 0 )); then
+	echo "Не найдено ни одного совпадения в файле $(realpath "$FILE")"
+else
+	echo "Количество найденных совпадений: $NUMBER_OF_LINES"
+fi
